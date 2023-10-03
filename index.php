024a458286194d1ca2f9582e8889061406d0d563
Gowthamap <gowthamap55555@gmail.com>
2023-10-03 12:31:37 +0530

    feat: variable naming

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$age = 20;
+$str = "Your age is $age";
+
+$name = '\'Anoop\'';
+
+echo $str;
+
+define("PI", 3.14);
+echo PI;
+
+
+$first_name = "Tintu";
+$middle_name = "C";
+$last_name = "Raju";
+$full_name = $first_name." ".$middle_name." ".$last_name;
+echo $first_name." ".$middle_name." ".$last_name;;
+
+
+ ?>
