5f2cd6a13bf0d3fc8b583c433fb6a6eeafa11ae2
Gowthamap <gowthamap55555@gmail.com>
2023-10-03 12:56:24 +0530

    feat: switch statement

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <?php
 
-$numbers = array(10,20);
-// echo $numbers[1];
+$a=4;
 
-// $ar = ["name" => "Tintu", "age"=> 28, "mark"=>40];
-array_push($numbers, 30);
-array_push($numbers, 30);
-array_push($numbers, 30);
-array_push($numbers, "this is a string");
+switch($a) {
+  case 1:  echo "Sunday";
+  break;
+  case 2: echo "Monday";
+  break;
+  case 3: echo "Tuesday";
+  break;
+  default: echo "None of the above";
+}
 
-
-var_dump($numbers);
-
-unset($numbers[5]);
-
-var_dump($numbers);
 
 ?>
