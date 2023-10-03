06474e4092cc0bd942ce5cffed018b297fff4e85
Gowthamap <gowthamap55555@gmail.com>
2023-10-03 15:00:55 +0530

    feat: object oriented php

---
+++ Object-oriented-php/tutorial.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class Student {
+private $name;
+private $roll_number;
+
+public function setName($n) {
+  $this->name = $n;
+}
+
+public function setRollNumber($r) {
+  $this->roll_number = $r;
+}
+
+public function getName() { 
+  return $this->name;
+}
+
+public function getRollNumber() {
+ return $this->roll_number;
+}
+
+}
+
+$ob = new Student();
+$ob->setName("appu");
+$ob->setRollNumber(10);
+
+echo $ob->getName();
+echo $ob->getRollNumber();
+
+ ?>
